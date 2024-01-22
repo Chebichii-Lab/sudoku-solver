38a97cac55fd3312abf6f2af1f3b0257612c223e
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-22 10:52:03 +0300

    jot down the steps

---
+++ sudoku.py
@@ -0,0 +1,8 @@
+def solve_sudoku(puzzle):
+    # solve sudoku with a backtracking technique
+    # our puzzle is a list of lists, where each inner list is a row in our sudoku puzzle
+    # return whether a solution exits
+    # mutates puzzle to be the solution  (if the solution exists)
+
+    # step 1: choose somewhere on the puzzle to make a guess
+    
