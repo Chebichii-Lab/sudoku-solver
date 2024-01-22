6318fe07bb762d8599e4d6cd6c9d591389e746eb
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-22 11:24:52 +0300

    create a function to check if guesses are valid

--- sudoku.py
+++ sudoku.py
@@ -10,6 +10,37 @@ def find_next_empty(puzzle):
             
     return None, None # if no spaces in the puzzle are empty (-1)
 
+def is_valid(puzzle, guess, row, col):
+    # figures if the guess at the row/col of the puzzle is valid
+    # return false if it is not, true if it is
+
+    # let's start with the row:
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False
+    
+    # columns:
+    # col_vals = []
+    # for i in range(9):
+    #     col_vals.append(puzzle[i] [col])
+    col_vals = [puzzle[i] for i in range(9)]
+    if guess in col_vals:
+        return False
+    
+    # and then the squeares
+    # we want to get where the 3x3 square starts
+    # and iterate over the 3 values in the row/column
+    row_start = (row // 3) * 3
+    col_start = (col // 3) * 3
+
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start + 3):
+            if puzzle[r][c] == guess:
+                return False
+            
+    # if we get here, these checks pass
+    return True
+
 
 def solve_sudoku(puzzle):
     # solve sudoku with a backtracking technique
@@ -19,3 +50,13 @@ def solve_sudoku(puzzle):
 
     # step 1: choose somewhere on the puzzle to make a guess
     row, col = find_next_empty(puzzle)
+
+    # step 1.1: if there's nowhere left, then we're done because we only allowed valid inputs
+    if row is None:
+        return True
+    
+    # step 2: if there us a place to put a number, then make a guess between 1 and 9
+    for guess in range(1, 10): # range (1, 10) is 1, 2, 3 ... 9
+        #step 3: check if this is valid guess
+        if is_valid(puzzle, guess, row, col):
+
